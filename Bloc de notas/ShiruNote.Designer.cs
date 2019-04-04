5f83bb8d5891c9839ea7b438f52e5e4a306bf925
Noruka <guillem.burnlees.viada@cieep.com>
2019-04-04 12:35:08 +0200

    Update GUI

---
+++ Bloc de notas/ShiruNote.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Bloc_de_notas
+{
+    partial class ShiruNote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.msMenuHorizontal = new System.Windows.Forms.MenuStrip();
+            this.crearNotaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.borrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tbTitulo = new System.Windows.Forms.TextBox();
+            this.rtbNota = new System.Windows.Forms.RichTextBox();
+            this.toolStripCBLista = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripCBBorrar = new System.Windows.Forms.ToolStripComboBox();
+            this.msMenuHorizontal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // msMenuHorizontal
+            // 
+            this.msMenuHorizontal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.crearNotaToolStripMenuItem,
+            this.guardarToolStripMenuItem,
+            this.editarToolStripMenuItem,
+            this.borrarToolStripMenuItem});
+            this.msMenuHorizontal.Location = new System.Drawing.Point(0, 0);
+            this.msMenuHorizontal.Name = "msMenuHorizontal";
+            this.msMenuHorizontal.Size = new System.Drawing.Size(460, 24);
+            this.msMenuHorizontal.TabIndex = 0;
+            this.msMenuHorizontal.Text = "menuStrip1";
+            // 
+            // crearNotaToolStripMenuItem
+            // 
+            this.crearNotaToolStripMenuItem.Name = "crearNotaToolStripMenuItem";
+            this.crearNotaToolStripMenuItem.Size = new System.Drawing.Size(76, 20);
+            this.crearNotaToolStripMenuItem.Text = "Crear Nota";
+            this.crearNotaToolStripMenuItem.Click += new System.EventHandler(this.CrearNotaToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.GuardarToolStripMenuItem_Click);
+            // 
+            // editarToolStripMenuItem
+            // 
+            this.editarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripCBLista});
+            this.editarToolStripMenuItem.Name = "editarToolStripMenuItem";
+            this.editarToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.editarToolStripMenuItem.Text = "Editar";
+            // 
+            // borrarToolStripMenuItem
+            // 
+            this.borrarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripCBBorrar});
+            this.borrarToolStripMenuItem.Name = "borrarToolStripMenuItem";
+            this.borrarToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.borrarToolStripMenuItem.Text = "Borrar";
+            // 
+            // tbTitulo
+            // 
+            this.tbTitulo.Location = new System.Drawing.Point(12, 27);
+            this.tbTitulo.Name = "tbTitulo";
+            this.tbTitulo.Size = new System.Drawing.Size(436, 20);
+            this.tbTitulo.TabIndex = 1;
+            // 
+            // rtbNota
+            // 
+            this.rtbNota.Location = new System.Drawing.Point(13, 54);
+            this.rtbNota.Name = "rtbNota";
+            this.rtbNota.Size = new System.Drawing.Size(435, 384);
+            this.rtbNota.TabIndex = 2;
+            this.rtbNota.Text = "";
+            // 
+            // toolStripCBLista
+            // 
+            this.toolStripCBLista.Name = "toolStripCBLista";
+            this.toolStripCBLista.Size = new System.Drawing.Size(121, 23);
+            this.toolStripCBLista.DropDownClosed += new System.EventHandler(this.ToolStripCBLista_DropDownClosed);
+            // 
+            // toolStripCBBorrar
+            // 
+            this.toolStripCBBorrar.Name = "toolStripCBBorrar";
+            this.toolStripCBBorrar.Size = new System.Drawing.Size(121, 23);
+            this.toolStripCBBorrar.DropDownClosed += new System.EventHandler(this.ToolStripCBBorrar_DropDownClosed);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 450);
+            this.Controls.Add(this.rtbNota);
+            this.Controls.Add(this.tbTitulo);
+            this.Controls.Add(this.msMenuHorizontal);
+            this.MainMenuStrip = this.msMenuHorizontal;
+            this.Name = "Form1";
+            this.Text = "ShiruNote 0.6";
+            this.msMenuHorizontal.ResumeLayout(false);
+            this.msMenuHorizontal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip msMenuHorizontal;
+        private System.Windows.Forms.ToolStripMenuItem crearNotaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem borrarToolStripMenuItem;
+        private System.Windows.Forms.TextBox tbTitulo;
+        private System.Windows.Forms.RichTextBox rtbNota;
+        private System.Windows.Forms.ToolStripComboBox toolStripCBLista;
+        private System.Windows.Forms.ToolStripComboBox toolStripCBBorrar;
+    }
+}
---
+++ Bloc de notas/ShiruNote.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Bloc_de_notas
+{
+    public partial class ShiruNote : Form
+    {
+        private List<ObjetoNota> Notas = new List<ObjetoNota>();
+
+        private ObjetoNota nota;
+
+        public ShiruNote()
+        {
+            InitializeComponent();
+        }
+
+        private void CrearNotaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ClearContenidos();
+        }
+
+        //Funcion que limpia los paneles de texto
+        private void ClearContenidos()
+        {
+            tbTitulo.Text = "";
+            rtbNota.Text = "";
+        }
+
+        //Comprueba si existe el objeto en el array
+        private Boolean ComprobarExiste(String t)
+        {
+            for (int i = 0; i < Notas.Count; i++)
+            {
+                if (Notas[i].Titulo.Equals(t))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        //Funcion para añadir notas al combobox.
+        private void AnyadirItemsCB(String titulo)
+        {
+            toolStripCBLista.Items.Add(titulo);
+            toolStripCBBorrar.Items.Add(titulo);
+        }
+
+        private void BorrarItemsCB(int pos)
+        {
+            Notas.RemoveAt(pos);
+            toolStripCBLista.Items.RemoveAt(pos);
+            toolStripCBBorrar.Items.RemoveAt(pos);
+        }
+
+        private void GuardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            String tituloNotaActual = tbTitulo.Text;
+            String contenidoNotaActual = rtbNota.Text;
+
+            if (tituloNotaActual.Equals(""))
+            {
+                MessageBox.Show("No has insertado titulo");
+            }
+            else if (ComprobarExiste(tituloNotaActual))
+            {
+                int pos = 0;
+
+                for (int i = 0; i < Notas.Count; i++)
+                {
+                    if (Notas[i].Titulo.Equals(tituloNotaActual))
+                    {
+                        pos = i;
+                        break;
+                    }
+                }
+
+                Notas[pos].SetNota(tituloNotaActual, contenidoNotaActual);
+                MessageBox.Show("Sobreescrito nota existente con titulo: " + tituloNotaActual);
+                ClearContenidos();
+            }
+            else
+            {
+                nota = new ObjetoNota(tituloNotaActual, contenidoNotaActual);
+                Notas.Add(nota);
+                AnyadirItemsCB(tituloNotaActual);
+                MessageBox.Show("Se ha creado la nota con titulo: " + tituloNotaActual);
+                ClearContenidos();
+            }
+        }
+
+        private void ToolStripCBLista_DropDownClosed(object sender, EventArgs e)
+        {
+            if (toolStripCBLista.Items.Count == 0)
+            {
+                MessageBox.Show("No hay nada guardado en la lista");
+            }
+            else
+            {
+                if (toolStripCBLista.SelectedIndex == -1)
+
+                {
+                    MessageBox.Show("Seleccione un contenido");
+                }
+                else
+                {
+                    if (ComprobarExiste(Notas[toolStripCBLista.SelectedIndex].Titulo))
+                    {
+                        ClearContenidos();
+                        tbTitulo.Text = Notas[toolStripCBLista.SelectedIndex].Titulo;
+                        rtbNota.Text = Notas[toolStripCBLista.SelectedIndex].Contenido;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El contenido seleccionado no existe");
+                    }
+                }
+            }
+        }
+
+        private void ToolStripCBBorrar_DropDownClosed(object sender, EventArgs e)
+        {
+            int i = toolStripCBBorrar.SelectedIndex;
+
+            if (toolStripCBLista.Items.Count != 0 && i != -1)
+            {
+                MessageBox.Show("Se ha borrado " + Notas[i].Titulo);
+                BorrarItemsCB(i);
+
+                //Limita el tamaño y limpia el texto del combobox.
+                if (toolStripCBLista.Items.Count != 0)
+                {
+                    toolStripCBLista.MaxDropDownItems = toolStripCBLista.Items.Count;
+                }
+                else
+                {
+                    toolStripCBLista.Text = "";
+                }
+
+                ClearContenidos();
+            }
+        }
+    }
+}
